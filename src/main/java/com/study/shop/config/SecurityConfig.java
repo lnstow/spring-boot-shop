fdda0d4a1d5d4be66de77856619dff7b1a32707a
lnstow <979071270@qq.com>
2020-06-10 21:26:27 +0800

    完成注册

--- src/main/java/com/study/shop/config/SecurityConfig.java
+++ src/main/java/com/study/shop/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.study.shop.config;
 
 import com.study.shop.service.impl.UserDetailsServiceImpl;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,105 +12,97 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    @Autowired
-//    private UserService userService;
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-         //this.inMemoryAuthentication(auth);
-         //this.jdbcAuthentication(auth);
+        // this.inMemoryAuthentication(auth);
+        // this.jdbcAuthentication(auth);
         this.userDetailAuthentication(auth);
-
     }
-//
-//    // 内存身份认证
-//    private void inMemoryAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//        auth.inMemoryAuthentication().passwordEncoder(encoder).withUser("test").password(encoder.encode("test"))
-//                .roles("common").and().withUser("admin").password(encoder.encode("admin")).roles("vip");
-//    }
-//
-//    @Autowired
-//    private DataSource dataSource;
 
-    // JDBC身份认证
-//    private void jdbcAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//
-//        String userSQL = "select Username,Password,Valid from Customer where Username = ?";
-//        String authoritySQL = "select c.username,a.authority from Customer c,Authority a,"
-//                + "CustomerAuthority ca where CustomerId=c.id " + "and ca.AuthorityId=a.id and c.username =?";
-//        String userSQL = "select Username,Password,email from user where Username = ?";
-//        String authoritySQL = "select Username, authority from user where Username = ?";
-//        auth.jdbcAuthentication().passwordEncoder(encoder)
-//                .dataSource(dataSource)
-//                .usersByUsernameQuery(userSQL)
-//                .authoritiesByUsernameQuery(authoritySQL);
-//    }
+    // // 内存身份认证
+    // private void inMemoryAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+    //     BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    //     auth.inMemoryAuthentication().passwordEncoder(encoder).withUser("test").password(encoder.encode("test"))
+    //             .roles("common").and().withUser("admin").password(encoder.encode("admin")).roles("vip");
+    // }
 
-     @Autowired
-     private UserDetailsServiceImpl userDetailsService;
+    // @Autowired
+    // private DataSource dataSource;
+
+    // // JDBC身份认证
+    // private void jdbcAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+    //     BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    //     String userSQL = "select Username,Password,Valid from Customer where Username = ?";
+    //     String authoritySQL = "select c.username,a.authority from Customer c,Authority a,"
+    //             + "CustomerAuthority ca where CustomerId=c.id " + "and ca.AuthorityId=a.id and c.username =?";
+    //     // String userSQL = "select Username,Password,email from user where Username =
+    //     // ?";
+    //     // String authoritySQL = "select Username, authority from user where Username =
+    //     // ?";
+    //     auth.jdbcAuthentication().passwordEncoder(encoder).dataSource(dataSource).usersByUsernameQuery(userSQL)
+    //             .authoritiesByUsernameQuery(authoritySQL);
+    // }
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
     private void userDetailAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-         auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
-
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         this.authorizeRequests(http);
         this.formLogin(http);
-        // this.logout(http);
-        // this.rememberMe(http);
-
+        this.logout(http);
+        this.rememberMe(http);
         http.csrf().disable();
     }
 
-   // 自定义用户授权管理
-   private void authorizeRequests(HttpSecurity http) throws Exception {
+    // 自定义用户授权管理
+    private void authorizeRequests(HttpSecurity http) throws Exception {
         // http.authorizeRequests().anyRequest().permitAll().and().logout().permitAll();
-       http.authorizeRequests()
-               // 放行
-               .antMatchers("/").permitAll()
-               // 需要对static文件夹下静态资源进行统一放行
-               .antMatchers("/index/**").permitAll()
-               .antMatchers("/login/**").permitAll()
-               .antMatchers("/test").permitAll()
-            //    .antMatchers("/detail/common/**").hasRole("common")
-            //    .antMatchers("/detail/vip/**").hasRole("vip")
-               .anyRequest().authenticated();
-   }
+        String[] requestPath = { "/", "/test", "/userRegister" };
+        String[] resources = { "/index/**", "/login/**" };
+        http.authorizeRequests()
+                // 放行
+                .antMatchers(requestPath).permitAll()
+                .antMatchers(resources).permitAll()
+                // 需要对static文件夹下静态资源进行统一放行
+                // .antMatchers("/detail/common/**").hasRole("common")
+                // .antMatchers("/detail/vip/**").hasRole("vip")
+                .anyRequest().authenticated();
+    }
 
-   // 自定义用户登录
-   private void formLogin(HttpSecurity http) throws Exception {
-       http.formLogin()
-       .loginPage("/userLogin").permitAll()
-       .usernameParameter("name").passwordParameter("pwd")
-               .defaultSuccessUrl("/").failureUrl("/userLogin?error");
-   }
+    // 自定义用户登录
+    private void formLogin(HttpSecurity http) throws Exception {
+        http.formLogin().loginPage("/userLogin").permitAll()
+        .usernameParameter("username").passwordParameter("password")
+        .defaultSuccessUrl("/").failureUrl("/userLogin?error");
+    }
 
-//    // 自定义用户退出控制
-//    private void logout(HttpSecurity http) throws Exception {
-//        http.logout().logoutUrl("/mylogout").logoutSuccessUrl("/");
-//    }
-//
-//    // 定制Remember-me记住我功能
-//    private void rememberMe(HttpSecurity http) throws Exception {
-//        http.rememberMe().rememberMeParameter("rememberme").tokenValiditySeconds(600)
-//                // 对cookie信息进行持久化管理
-//                .tokenRepository(tokenRepository());
-//    }
-//
-//    /**
-//     * 持久化Token存储
-//     *
-//     * @return
-//     */
-//    @Bean
-//    public JdbcTokenRepositoryImpl tokenRepository() {
-//        JdbcTokenRepositoryImpl jr = new JdbcTokenRepositoryImpl();
-//        jr.setDataSource(dataSource);
-//        return jr;
-//    }
+    // 自定义用户退出控制
+    private void logout(HttpSecurity http) throws Exception {
+        http.logout().logoutUrl("/mylogout").logoutSuccessUrl("/");
+    }
+
+    // 定制Remember-me记住我功能
+    private void rememberMe(HttpSecurity http) throws Exception {
+        http.rememberMe().rememberMeParameter("rememberme").tokenValiditySeconds(600);
+                // 对cookie信息进行持久化管理
+                // .tokenRepository(tokenRepository());
+    }
+
+    // /**
+    //  * 持久化Token存储
+    //  *
+    //  * @return
+    //  */
+    // @Bean
+    // public JdbcTokenRepositoryImpl tokenRepository() {
+    //     JdbcTokenRepositoryImpl jr = new JdbcTokenRepositoryImpl();
+    //     jr.setDataSource(dataSource);
+    //     return jr;
+    // }
 }
--- src/main/java/com/study/shop/controller/LoginController.java
+++ src/main/java/com/study/shop/controller/LoginController.java
@@ -1,10 +1,19 @@
 package com.study.shop.controller;
 
+import com.study.shop.domain.User;
+import com.study.shop.service.impl.UserServiceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    UserServiceImpl userService;
 
     // 向用户登录页面跳转
     @GetMapping("/userLogin")
@@ -16,6 +25,20 @@ public class LoginController {
     @GetMapping("/userRegister")
     public String toRegister() {
         return "register/register";
+    }
+
+    @PostMapping(value = "/userRegister")
+    public String addUser(User user, String okpwd) {
+        if (!user.getPassword().equals(okpwd))
+            return "redirect:/userRegister";
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        try {
+            // 重复注册会抛出异常
+            userService.save(user);
+        } catch (Exception e) {
+            return "redirect:/userRegister";
+        }
+        return "redirect:/";
     }
 
 }
