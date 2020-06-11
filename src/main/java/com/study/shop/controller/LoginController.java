a57e55fe65817cb23b2cafbda2304f9ff1f09765
lnstow <979071270@qq.com>
2020-06-11 15:36:13 +0800

    增加注册失败提示信息

--- src/main/java/com/study/shop/controller/LoginController.java
+++ src/main/java/com/study/shop/controller/LoginController.java
@@ -6,8 +6,10 @@ import com.study.shop.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
@@ -23,19 +25,26 @@ public class LoginController {
 
     // 向用户注册页面跳转
     @GetMapping("/userRegister")
-    public String toRegister() {
+    public String toRegister(Model model) {
+        model.addAttribute("error", model.getAttribute("error"));
+        model.addAttribute("user", model.getAttribute("user"));
         return "register/register";
     }
 
     @PostMapping(value = "/userRegister")
-    public String addUser(User user, String okpwd) {
-        if (!user.getPassword().equals(okpwd))
+    public String addUser(User user, String okpwd, RedirectAttributes attr) {
+        if (!user.getPassword().equals(okpwd)) {
+            attr.addFlashAttribute("error", "两次输入的密码不一样！");
+            attr.addFlashAttribute("user", user);
             return "redirect:/userRegister";
+        }
         user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
         try {
             // 重复注册会抛出异常
             userService.save(user);
         } catch (Exception e) {
+            attr.addFlashAttribute("error", "用户名已注册，请更换！");
+            attr.addFlashAttribute("user", user);
             return "redirect:/userRegister";
         }
         return "redirect:/";
